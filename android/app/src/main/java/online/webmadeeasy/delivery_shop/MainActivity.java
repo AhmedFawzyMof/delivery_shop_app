1ff416b14b754685f102ca36e52d9c1844087ffa
Ahmed Fawzi <85083711+AhmedFawzyMof@users.noreply.github.com>
2025-11-02 16:41:36 +0200

    fix: forground notifcation

--- android/app/src/main/java/online/webmadeeasy/delivery_shop/MainActivity.java
+++ android/app/src/main/java/online/webmadeeasy/delivery_shop/MainActivity.java
@@ -1,5 +1,13 @@
 package online.webmadeeasy.delivery_shop;
 
 import com.getcapacitor.BridgeActivity;
+import cordova.plugin.foreground.service.ForegroundServicePlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onStart() {
+        super.onStart();
+        // ⬇️ Register the Foreground Service plugin manually
+        registerPlugin(ForegroundServicePlugin.class);
+    }
+}
