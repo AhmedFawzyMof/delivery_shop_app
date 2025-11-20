b6c9f28d99b365a7caaba5b35c132fa21c2ffa09
AhmedFawzyMof <np1265448@gmail.com>
2025-11-20 21:44:15 +0200

    add: interceptors for seesionToken

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,3 +1,4 @@
+import { Preferences } from "@capacitor/preferences";
 import axios from "axios";
 
 export const baseUrl = "https://deliveryshop.cloud/api";
@@ -6,4 +7,25 @@ const api = axios.create({
   baseURL: baseUrl,
 });
 
+api.interceptors.request.use(
+  async (config) => {
+    try {
+      const { value: sessionToken } = await Preferences.get({
+        key: "sessionToken",
+      });
+
+      if (sessionToken) {
+        config.headers = config.headers || {};
+        config.headers.Authorization = `Bearer ${sessionToken}`;
+      }
+    } catch (err) {
+      console.warn("Failed to load sessionToken from Preferences:", err);
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 export default api;
